a39238851ab3b3c305b90964fe9b6d9ab36e1236
hafsamannan04 <hafsamannan04@gmail.com>
2026-03-25 16:01:32 +0400

    Exercise 2 .py

---
+++ Exercise 2 .py
@@ -0,0 +1,8 @@
+# declaring integer variables x and y 
+x=8
+y=10
+# adding the values of both integer 
+# and storing the result in variable p
+p=x+y 
+# displaying the final value stored in p 
+print(p)
