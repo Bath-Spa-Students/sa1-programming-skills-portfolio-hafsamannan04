b52ea6d8cc60498d235930c793457517493c329e
hafsamannan04 <hafsamannan04@gmail.com>
2026-03-27 18:49:22 +0400

    Exercise 7 .py

---
+++ Exercise 7 .py
@@ -0,0 +1,22 @@
+# some counting 
+# using for loop and range
+
+#1
+for x in range(0, 51 ):
+    print(x)
+    
+#2
+for y in range(50, -1 ,-1 ):
+    print(y)
+
+#3
+for z in range(30, 51 ,1 ):
+    print(z)
+
+#4
+for a in range(50, 9 ,-2 ):
+    print(a)
+#5
+for b in range(100, 201, 5 ):
+    print(b)
+    
