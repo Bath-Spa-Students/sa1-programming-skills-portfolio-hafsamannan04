333dcadbdcf8bffd174adfdd9717ff308019e8a8
hafsamannan04 <hafsamannan04@gmail.com>
2026-03-27 18:45:56 +0400

    Exercise 3 .py

--- Exercise 3 .py
+++ Exercise 3 .py
@@ -3,8 +3,8 @@
 #step 1
 #storing info in dict as key-value pairs
 Bio_info={
-    "name": "Hafsa",
-    "hometown": "Sharjah",
+    "name":"Hafsa",
+    "hometown":"Sharjah",
     "age":21
 }
 #step 2
@@ -20,9 +20,9 @@ hometown="Sharjah"
 age=21
 
 #storing data in dict with variables
-Bio_info={"name": name,   # string
-    "hometown": hometown, # string
-    "age": age            # integer
+Bio_info={"name":name,   
+    "hometown": hometown, 
+    "age": age           
 }
 for value in Bio_info.values():
     print(value)
@@ -31,11 +31,10 @@ for value in Bio_info.values():
 
 # advance requirement
 # create a dict storing key and empty value 
-info={"Name:":"","Hometown:":"","Age:":""} 
-info["Name:"]=input("Enter your name: " )
-info["Hometown:"]=input("Enter your place of birth: " )
-info["Age:"]=input("Enter your age: ") # storing as string so twenty one can be entered
+info= {"Name:":" ","Hometown:":" ","Age:":""} 
+info ["Name:"] =input("Enter your name: " )
+info ["Hometown:"] =input("Enter your place of birth: " )
+info ["Age:"] =input("Enter your age: ")  # storing as string so twenty one can be entered
 for key,value in info.items():
     print(key + value)
-
 
